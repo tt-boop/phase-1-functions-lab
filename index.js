3c263b28ed0150b9ff79833713cf4cdcf967d9a2
Ayomide Taiwo <tosin0330@gmail.com>
2023-04-05 00:36:35 -0500

    myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(numOfBlocks) {
+    if (numOfBlocks > 42) {
+      return numOfBlocks - 42;
+    } else {
+      return 42 - numOfBlocks;
+    }
+  }
+  
+  function distanceFromHqInFeet(numOfBlocks) {
+    let distanceInBlocks = distanceFromHqInBlocks(numOfBlocks);
+    let distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = Math.abs(destination - start);
+    let feetTravelled = blocksTravelled * 264;
+    return feetTravelled;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+  
+  
